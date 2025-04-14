c40cf3caa61208e6fc920403b10d5512ae26244a
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-04-14 22:03:20 +0200

    add . functionality

--- javascript.js
+++ javascript.js
@@ -6,23 +6,20 @@ let number2 = null;
 let currentNumber = 0;
 let newNumber = true;
 let currentBtn = null;
+let dot = false;
 
-const buttons = document.querySelectorAll("button");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 const resetButton = document.querySelector(".reset-btn");
 const displayNumber = document.querySelector("#display-number");
-//---- Event Listeners----//
-for(const btn of buttons){
-    //btn.addEventListener("click", () => updateCurrentOperation(btn));
-}
 
+//---- Event Listeners----//
 for(const btn of numberButtons){
     btn.addEventListener("click", () => numberBtnClicked(btn));
 }
 
 for(const btn of operatorButtons){
-    btn.addEventListener("click", () => updateOperator(btn));
+    btn.addEventListener("click", () => operatorBtnClicked(btn));
 }
 
 resetButton.addEventListener("click", clear);
@@ -30,7 +27,14 @@ resetButton.addEventListener("click", clear);
 //---- Functionality ---- //
 function numberBtnClicked(btn){
     const value = btn.value;
+
+    if(value === "."){
+        dotBtnClicked();
+        return;
+    }
+
     let number;
+
     if (newNumber === true){
         if(value == 0){
             updateValue(0);
@@ -38,25 +42,40 @@ function numberBtnClicked(btn){
         number = value;
         newNumber = false;
 
-        //sets number 2 to something as we check if number2 is null
+        //we need a 2nd input and this acts like a bool that checks if we have a 2nd input or not
         if(number1 != null){
             number2 = number;
         }
     } 
-    //keep adding numbers
     else{
-        number = currentNumber == 0 ? value : currentNumber + value;
+        //fix so its 1 instead of 01 etc.
+        number = currentNumber === 0 ? value : currentNumber + value;
     } 
-    console.log(currentNumber);
     if(number == 0) return;
     updateValue(number);
 }
+
+function dotBtnClicked(){
+    if(dot) return;
+    let value;
+    if(newNumber === true){
+        value = "0.";
+        newNumber = false;
+    }
+    else{
+        value = currentNumber + ".";
+    }
+    dot = true;
+    updateValue(value);
+}
+
 function updateValue(val){
     currentNumber = val;
     displayNumber.textContent = currentNumber;
+    console.log(currentNumber);
 }
 
-function updateOperator(btn){
+function operatorBtnClicked(btn){
     const value = btn.value;
 
     if (value === "="){
@@ -66,9 +85,11 @@ function updateOperator(btn){
         operator = null;
         number1 = null;
         number2 = null;
+        dot = false;
         updateCurrentOperation();
     }
 
+    //if we are on 1st input
     else if(number2 === null){
         number1 = currentNumber;
         operator = value;
@@ -76,15 +97,17 @@ function updateOperator(btn){
         console.log(value)
     }
 
-    else if(number2 != null){
+    //if we are on 2nd input
+    else{
         updateValue(operate(number1, currentNumber, operator));
         number1 = currentNumber;
         operator = value;
         updateCurrentOperation(btn);
         number2 = null;
+        dot = false;
         
     }
-    
+
     newNumber = true;
 }
 
@@ -110,6 +133,7 @@ function clear(){
     operator = null;
     number1 = null;
     number2 = null;
+    dot = false;
     updateCurrentOperation();
 }
 
@@ -127,7 +151,7 @@ function divide(a, b){
     return a / b;
 }
 
-
+//---- Effects ----//
 function updateCurrentOperation(btn = null){
     if(currentBtn !== null){
         const colorOld = window.getComputedStyle(currentBtn).color;
