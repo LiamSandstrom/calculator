2583dcd5910ddfa3d0410e7b590096fca4aee845
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-04-14 16:18:17 +0200

    add function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-console.log("hi");
+
+//---- Variables ---- //
+//number 1
+//operator
+//number 2
+
+
+
+
+
+
+
+
+//---- Functionality ---- //
+//operate
+//add
+function add(a, b){
+    return a + b;
+}
+//subtract
+//multiply
+//divide
+
+//clear
+
